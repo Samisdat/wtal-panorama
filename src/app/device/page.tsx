97523d3b6eaded371df70bfd4f7e70ee7a808350
Bastian <Bastian.Pertz1@Vodafone.com>
2026-02-19 23:55:03 +0100

    start a css 3d device

---
+++ src/app/device/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { DigitalClock } from '@/components/Clock/DigitalClock';
+import { TimeFactor } from '@/components/Control/TimeFactor';
+import { DeviceorientationIcon } from '@/components/DeviceorientationIcon';
+import { Panorama } from '@/components/Panorama';
+import { TalProvider } from '@/lib/TalContext';
+import styles from './page.module.css';
+
+export default function Home() {
+    return (
+        <>
+            <DeviceorientationIcon />
+            <TalProvider>
+                <div className={styles.page}>
+                    <main className={styles.main}>
+                        <Panorama />
+                        <DigitalClock />
+                        <TimeFactor />
+                    </main>
+                </div>
+            </TalProvider>
+
+            <main>
+                <h1>Lorem ipsum</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </main>
+        </>
+    );
+}
---
+++ src/components/DeviceorientationIcon/index.tsx
@@ -0,0 +1,79 @@
+import { ChangeEvent, CSSProperties, useState } from 'react';
+
+export const DeviceorientationIcon = () => {
+    const [x, setX] = useState(45);
+
+    const [y, setY] = useState(0);
+    const [z, setZ] = useState(45);
+
+    const onXChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setX(Number(event.target.value));
+    };
+
+    const onYChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setY(Number(event.target.value));
+    };
+
+    const onZChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setZ(Number(event.target.value));
+    };
+
+    return (
+        <>
+            <label>
+                <input
+                    type="range"
+                    value={x}
+                    onChange={onXChange}
+                    min={0}
+                    max={360}
+                    step={1}
+                />
+                X
+            </label>
+            <label>
+                <input
+                    type="range"
+                    value={y}
+                    onChange={onYChange}
+                    min={0}
+                    max={360}
+                    step={1}
+                />
+                y
+            </label>
+            <label>
+                <input
+                    type="range"
+                    value={z}
+                    onChange={onZChange}
+                    min={0}
+                    max={360}
+                    step={1}
+                />
+                z
+            </label>
+            <div
+                className="device-orientation-icon-wrapper"
+                style={
+                    {
+                        ['--x-axis' as string]: String(x),
+                        ['--y-axis' as string]: String(y),
+                        ['--z-axis' as string]: String(z),
+                    } as CSSProperties
+                }
+            >
+                <div className="device-orientation-icon">
+                    <div className="device">
+                        <div className="side front">front</div>
+                        <div className="side back">right</div>
+                        <div className="side top">back</div>
+                        <div className="side bottom">top</div>
+                        <div className="side left">left</div>
+                        <div className="side right">bottom</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
